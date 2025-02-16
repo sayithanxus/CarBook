6022c7a38838ef3b710c84d6ad376bd96118a8b4
Sayithan Usta <sayit2828@hotmail.com>
2025-02-16 23:32:55 +0300

    car create page complated

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,9 @@
-﻿using CarBook.Dto.CarDto;
+﻿using CarBook.Dto.BrandDto;
+using CarBook.Dto.CarDto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -19,21 +22,40 @@ namespace CarBook.WebUI.Controllers
             var responseMessage = await client.GetAsync("https://localhost:7173/api/Cars/GetCarWithBrand");
             if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData =await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
                 return View(values);
             }
             return View();
         }
+        [HttpGet]
         public async Task<IActionResult> CreateCar()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7173/api/Brands");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.name,
+                                                    Value = x.brandId.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7173/api/Cars",stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
-                return View(values);
+                return RedirectToAction("Index");
             }
             return View();
         }
