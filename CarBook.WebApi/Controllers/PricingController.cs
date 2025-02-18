015f96c8085562abe39086e306b3356933a8bc65
Sayithan Usta <sayit2828@hotmail.com>
2025-02-18 22:53:22 +0300

    added pricing type,comment,contact pages

--- CarBook.WebApi/Controllers/PricingController.cs
+++ CarBook.WebApi/Controllers/PricingController.cs
@@ -22,7 +22,7 @@ namespace CarBook.WebApi.Controllers
 			var values = await _mediator.Send(new GetPricingQuery());
 			return Ok(values);
 		}
-		[HttpGet("id")]
+		[HttpGet("{id}")]
 		public async Task<IActionResult> GetPricing(int id)
 		{
 			var values = await _mediator.Send(new GetPricingByIdQuery(id));
@@ -34,7 +34,7 @@ namespace CarBook.WebApi.Controllers
 			await _mediator.Send(command);
 			return Ok("Başarıyla Eklendi");
 		}
-		[HttpDelete]
+		[HttpDelete("{id}")]
 		public async Task<IActionResult> RemovePricing(int id)
 		{
 			await _mediator.Send(new RemovePricingCommand(id));
