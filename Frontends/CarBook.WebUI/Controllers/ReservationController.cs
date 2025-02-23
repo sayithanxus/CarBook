c01e658e761000bd09281b6f755947dbbde716f6
Sayithan Usta <sayit2828@hotmail.com>
2025-02-24 00:25:19 +0300

    complated images

--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,8 +1,10 @@
-﻿using CarBook.Dto.LocationDto;
+﻿using CarBook.Dto.CarDto;
+using CarBook.Dto.LocationDto;
 using CarBook.Dto.ReservationDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using System.Text;
 
@@ -21,21 +23,31 @@ namespace CarBook.WebUI.Controllers
             ViewBag.v1 = "Araç Kiralama";
             ViewBag.v2 = "Araç Rezervasyon Formu";
             ViewBag.v3 = id;
-
-
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7173/api/Locations");
 
-            var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
-            List<SelectListItem> values2 = (from x in values
-                                            select new SelectListItem
-                                            {
-                                                Text = x.Name,
-                                                Value = x.LocationID.ToString()
-                                            }).ToList();
-            ViewBag.v = values2;
+            var carResponseMessage = await client.GetAsync($"https://localhost:7173/api/Cars/{id}");
+            if (carResponseMessage.IsSuccessStatusCode)
+            {
+                var carJsonData = await carResponseMessage.Content.ReadAsStringAsync();
+                var carValue = JsonConvert.DeserializeObject<ResultCarWithBrandDto>(carJsonData);
+                CreateReservationDto dto = new CreateReservationDto();
+                dto.CarID = id;
+                ViewBag.car = $"{carValue.BrandName} {carValue.Model}";
+                ViewBag.carImage = carValue.CoverImgUrl;
 
+                var client1 = _httpClientFactory.CreateClient();
+                var responseMessage1 = await client1.GetAsync("https://localhost:7173/api/Locations");
+
+                var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+                var values1 = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData1);
+                List<SelectListItem> values3 = (from x in values1
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.LocationID.ToString()
+                                                }).ToList();
+                ViewBag.v = values3;
+            }
             return View();
         }
 
